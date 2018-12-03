6704b5f0c74947d519a422f7ec0ca6b8921523b7
krasodomska <39217554+krasodomska@users.noreply.github.com>
2018-12-03 15:32:34 +0100

    Update ControlsAndMechanics.h

--- DoNotStepOnSnake/DoNotStepOnSneak/ControlsAndMechanics.h
+++ DoNotStepOnSnake/DoNotStepOnSneak/ControlsAndMechanics.h
@@ -2,13 +2,14 @@
 class ControlsAndMechanics
 {
 public:
+	//how ofen snake will step forward
+	Uint32 stepTime = SDL_GetTicks();
+	//snake moving direction
 	enum directions { left, right, top, down };
 	//check if snake can move in choosed direction
 	bool ableToMove(int direction); 
 	void controls();
 	void move();
-	//how ofen snake will step forward
-	Uint32 stepTime = SDL_GetTicks();
 	bool tailBite();
 	bool stepOnApple();
 	void createNextApple();
